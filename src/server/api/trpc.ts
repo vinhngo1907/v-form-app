02c3dc34b0c964b72f6963d169fda8d12cf4af85
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-09-04 16:51:57 +0700

    [commit] - commit

---
+++ src/server/api/trpc.ts
@@ -0,0 +1,16 @@
+/**
+ * YOU PROBABLY DON'T NEED TO EDIT THIS FILE, UNLESS:
+ * 1. You want to modify request context (see Part 1).
+ * 2. You want to create a new middleware or type of procedure (see Part 3).
+ *
+ * TL;DR - This is where all the tRPC server stuff is created and plugged in. The pieces you will
+ * need to use are documented accordingly near the end.
+ */
+
+/**
+ * 1. CONTEXT
+ *
+ * This section defines the "contexts" that are available in the backend API.
+ *
+ * These allow you to access things when processing a request, like the database, the session, etc.
+ */
